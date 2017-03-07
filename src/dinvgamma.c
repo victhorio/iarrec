1710dfa1b2113102005579cf05d97fa96c7ee50a
vsart <vct.sartor@gmail.com>
2017-03-07 20:26:24 -0300

    Finished cleaning up

--- src/dinvgamma.c
+++ src/dinvgamma.c
@@ -6,7 +6,7 @@
  * Implements the density function for the inverse gamma distribution, under the R
  * interfaceable wrapper `dinvgamma` and under the C implementation `c_dinvgamma`.
  * Implementation of the R function and docstring definition can be found in the R
- * folder in the file `invgamma.R`
+ * folder in the file `invgamma.R`.
  */
 
 #include "dinvgamma.h"
@@ -27,8 +27,8 @@
 HOTFUNC void
 c_dinvgamma(double const *restrict x, R_xlen_t n, double a, double b, double *restrict r)
 {
-    for (R_xlen_t i = 0; i < n; i++)
-        r[i] = exp(a * log(b) - lgamma(a) - (a+1) * log(x[i]) - b/x[i]);
+	for (R_xlen_t i = 0; i < n; i++)
+		r[i] = exp(a * log(b) - lgamma(a) - (a+1) * log(x[i]) - b/x[i]);
 }
 
 
@@ -41,49 +41,49 @@ c_dinvgamma(double const *restrict x, R_xlen_t n, double a, double b, double *re
 SEXP
 dinvgamma(SEXP rx, SEXP ra, SEXP rb)
 {
-    int cond_protect = 0; // Number of conditional PROTECT calls
+	int cond_protect = 0; // Number of conditional PROTECT calls
 
 	// ---------------------------------------------
 	// Argument validation
 	// ---------------------------------------------
 
-    R_xlen_t n = Rf_xlength(rx);
-    if (n <= 0) {
-        Rf_error("First argument is empty.");
-    }
+	R_xlen_t n = Rf_xlength(rx);
+	if (n <= 0) {
+		Rf_error("First argument is empty.");
+	}
 
-    // If an INTSXP vector was received, coerce a REALSXP one into cx
-    // If an REALSXP vector was received, alias cx to it
-    // Otherwise raise an error
-    SEXP cx;
-    if (TYPEOF(rx) == INTSXP) {
-        cx = PROTECT(Rf_coerceVector(rx, REALSXP));
-        cond_protect++;
-    } else if (TYPEOF(rx) == REALSXP) {
-        cx = rx;
-    } else {
-        Rf_error("Firt argument is not numeric.");
-    }
+	// If an INTSXP vector was received, coerce a REALSXP one into cx
+	// If an REALSXP vector was received, alias cx to it
+	// Otherwise raise an error
+	SEXP cx;
+	if (TYPEOF(rx) == INTSXP) {
+		cx = PROTECT(Rf_coerceVector(rx, REALSXP));
+		cond_protect++;
+	} else if (TYPEOF(rx) == REALSXP) {
+		cx = rx;
+	} else {
+		Rf_error("Firt argument is not numeric.");
+	}
 
-    // Make sure second and third arguments are REALSXP, atomic and positive
-    if (Rf_xlength(ra) != 1 || TYPEOF(ra) != REALSXP || REAL(ra)[0] <= 0)
-        Rf_error("Expected second argument to be real valued, atomic and positive.");
-    if (Rf_xlength(rb) != 1 || TYPEOF(rb) != REALSXP || REAL(rb)[0] <= 0)
-        Rf_error("Expected third argument to be real valued, atomic and positive.");
+	// Make sure second and third arguments are REALSXP, atomic and positive
+	if (Rf_xlength(ra) != 1 || TYPEOF(ra) != REALSXP || REAL(ra)[0] <= 0)
+		Rf_error("Expected second argument to be real valued, atomic and positive.");
+	if (Rf_xlength(rb) != 1 || TYPEOF(rb) != REALSXP || REAL(rb)[0] <= 0)
+		Rf_error("Expected third argument to be real valued, atomic and positive.");
 
-    // ------------------------------------------
-    // Function logic
-    // ------------------------------------------
+	// ------------------------------------------
+	// Function logic
+	// ------------------------------------------
 
-    SEXP ret = PROTECT(Rf_allocVector(REALSXP, n));
+	SEXP ret = PROTECT(Rf_allocVector(REALSXP, n));
 
-    double const *const x = REAL(cx);
-    double *const r = REAL(ret);
-    double const a = REAL(ra)[0];
-    double const b = REAL(rb)[0];
+	double const *const x = REAL(cx);
+	double *const r = REAL(ret);
+	double const a = REAL(ra)[0];
+	double const b = REAL(rb)[0];
 
-    c_dinvgamma(x, n, a, b, r); // Call the actual implementation
+	c_dinvgamma(x, n, a, b, r);
 
-    UNPROTECT(1 + cond_protect);
-    return ret;
+	UNPROTECT(1 + cond_protect);
+	return ret;
 }
--- src/linvgamma.c
+++ src/linvgamma.c
@@ -27,8 +27,8 @@
 HOTFUNC void
 c_linvgamma(double const *restrict x, R_xlen_t n, double a, double b, double *restrict r)
 {
-    for (R_xlen_t i = 0; i < n; i++)
-        r[i] = a * log(b) - lgamma(a) - (a+1) * log(x[i]) - b/x[i];
+	for (R_xlen_t i = 0; i < n; i++)
+		r[i] = a * log(b) - lgamma(a) - (a+1) * log(x[i]) - b/x[i];
 }
 
 
@@ -41,41 +41,49 @@ c_linvgamma(double const *restrict x, R_xlen_t n, double a, double b, double *re
 SEXP
 linvgamma(SEXP rx, SEXP ra, SEXP rb)
 {
-    int np = 0; // Number of PROTECT calls
+	int cond_protect = 0; // Number of conditional PROTECT calls
 
-    // Make sure rx is non-empty
-    R_xlen_t n = Rf_xlength(rx);
-    if (n <= 0) {
-        Rf_error("First argument is empty.");
-    }
+	// ---------------------------------------------
+	// Argument validation
+	// ---------------------------------------------
 
-    // Make sure cx is a REALSXP vector
-    SEXP cx;
-    if (TYPEOF(rx) == INTSXP) {
-        cx = PROTECT(Rf_coerceVector(rx, REALSXP));
-        np++;
-    } else if (TYPEOF(rx) == REALSXP) {
-        cx = rx;
-    } else {
-        Rf_error("Firt argument is not numeric.");
-    }
+	R_xlen_t n = Rf_xlength(rx);
+	if (n <= 0) {
+		Rf_error("First argument is empty.");
+	}
 
-    // Make sure second and third arguments are REALSXP, atomic and positive
-    if (Rf_xlength(ra) != 1 || TYPEOF(ra) != REALSXP || REAL(ra)[0] <= 0)
-        Rf_error("Expected second argument to be real valued, atomic and strictly positive.");
-    if (Rf_xlength(rb) != 1 || TYPEOF(rb) != REALSXP || REAL(ra)[0] <= 0)
-        Rf_error("Expected third argument to be real valued, atomic and strictly positive.");
+	// If an INTSXP vector was received, coerce a REALSXP one into cx
+	// If an REALSXP vector was received, alias cx to it
+	// Otherwise raise an error
+	SEXP cx;
+	if (TYPEOF(rx) == INTSXP) {
+		cx = PROTECT(Rf_coerceVector(rx, REALSXP));
+		cond_protect++;
+	} else if (TYPEOF(rx) == REALSXP) {
+		cx = rx;
+	} else {
+		Rf_error("Firt argument is not numeric.");
+	}
 
-    SEXP ret = PROTECT(Rf_allocVector(REALSXP, n));
-    np++;
+	// Make sure second and third arguments are REALSXP, atomic and positive
+	if (Rf_xlength(ra) != 1 || TYPEOF(ra) != REALSXP || REAL(ra)[0] <= 0)
+		Rf_error("Expected second argument to be real valued, atomic and positive.");
+	if (Rf_xlength(rb) != 1 || TYPEOF(rb) != REALSXP || REAL(rb)[0] <= 0)
+		Rf_error("Expected third argument to be real valued, atomic and positive.");
 
-    double const *const x = REAL(cx);
-    double *const r = REAL(ret);
-    double const a = REAL(ra)[0];
-    double const b = REAL(rb)[0];
+	// ------------------------------------------
+	// Function logic
+	// ------------------------------------------
 
-    c_linvgamma(x, n, a, b, r);
+	SEXP ret = PROTECT(Rf_allocVector(REALSXP, n));
 
-    UNPROTECT(np);
-    return ret;
+	double const *const x = REAL(cx);
+	double *const r = REAL(ret);
+	double const a = REAL(ra)[0];
+	double const b = REAL(rb)[0];
+
+	c_linvgamma(x, n, a, b, r);
+
+	UNPROTECT(1 + cond_protect);
+	return ret;
 }
