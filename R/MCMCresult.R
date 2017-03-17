499a2f638bfe2485a7c0815c17992b73a1073cc4
vsart <vct.sartor@gmail.com>
2017-03-17 16:38:42 -0300

    More improvements and documentation to plot.MCMCresult

--- R/MCMCresult.R
+++ R/MCMCresult.R
@@ -7,16 +7,22 @@ print.MCMCresult <- function(x)
 				length(x[[1]]), x[[3]]))
 
 
-# @TODO: Add documentation
-#' plot.MCMCresult
+#' Plots a MCMC Result from the IARREC package
+#'
+#' @param x An MCMCresult S3 object
+#' @param drop Indicates how much of the samples to ignore.
+#'     If it's a value between [0,1) then it ignores the first \eqn{drop * n} samples from
+#'     the result. If it's a value within [1, +Inf) then the first \eqn{drop} values are
+#'     ignored. Ignores nothing by default.
+#' @param ... Optional base R graphical parameters
 #'
 #' @export
-plot.MCMCresult <- function(x, drop = 0)
+plot.MCMCresult <- function(x, drop = 0, ...)
 {
 	if (!is.numeric(drop) || drop < 0)
 		stop("Expected drop argument to be within [0,1] or [1, +Inf]")
 
-	if (drop.half < 1) {
+	if (drop < 1) {
 		n <- length(x$theta_1)
 		cutoff <- floor(n * drop) + 1
 		x$theta_1 <- x$theta_1[cutoff:n]
@@ -30,6 +36,9 @@ plot.MCMCresult <- function(x, drop = 0)
 	qt1 <- quantile(x$theta_1, c(0.25, 0.5, 0.75))
 	qt2 <- quantile(x$theta_2, c(0.25, 0.5, 0.75))
 
+	old.pars <- par(no.readonly = TRUE)
+	par(...)
+
 	layout(rbind(c(1,2), c(3, 4)))
 	plot(x$theta_1, type = 'l', main = 'Chain for Theta 1', ylab = '')
 	abline(h = qt1, col = 'orange', lty = 2, lwd = 1.5)
@@ -39,4 +48,6 @@ plot.MCMCresult <- function(x, drop = 0)
 	abline(v = qt1, col = 'orange', lty = 2, lwd = 1.5)
 	plot(density(x$theta_2), main = 'Density Estimate of Theta 2')
 	abline(v = qt2, col = 'orange', lty = 2, lwd = 1.5)
+
+	par(old.pars)
 }
